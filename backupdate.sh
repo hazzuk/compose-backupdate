bf392f2ff4f6e9a92a3af156cd2b90e64dfa2a5a
hazzuk <git@hazz.uk>
2024-11-03 16:41:30 +0000

    chore: update copyright notice

--- backupdate.sh
+++ backupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 version="1.1.0"
 
-# Copyright (c) 2024 hazzuk. All rights reserved.
+# Copyright (c) 2024 hazzuk
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
